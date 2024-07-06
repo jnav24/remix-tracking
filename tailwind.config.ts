7720ab4d81c22d67c8dea2439b88bdef6d34ac50
Justin Navarro <info@justinnavarro.net>
2024-07-06 11:41:08 -0400

    reformat the files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 import colors from 'tailwindcss/colors';
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        danger: colors.red["700"],
-        'danger-hover': colors.red["600"],
-        'danger-focus': colors.red["800"],
-        primary: colors.orange["400"],
-        'primary-hover': colors.orange["300"],
-        'primary-focus': colors.orange["500"],
-        success: colors.green["600"],
-      },
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      width: {
-        100: '25rem',
-        150: '38rem',
-        200: '50rem',
-      },
+    content: ['./app/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.red['700'],
+                'danger-hover': colors.red['600'],
+                'danger-focus': colors.red['800'],
+                primary: colors.orange['400'],
+                'primary-hover': colors.orange['300'],
+                'primary-focus': colors.orange['500'],
+                success: colors.green['600'],
+            },
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+            width: {
+                100: '25rem',
+                150: '38rem',
+                200: '50rem',
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 } satisfies Config;
