3199b5a514b1d8edf827a94bdd08b3af15625d47
Justin Navarro <info@justinnavarro.net>
2024-07-16 00:25:54 -0400

    update the auth layout with dark/light mode support

--- app/components/layouts/AuthLayout.tsx
+++ app/components/layouts/AuthLayout.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import RadarIcon from '~/components/icons/RadarIcon';
+import Typography from '~/components/elements/Typography';
 
 type Props = {
     children: React.ReactNode;
@@ -7,17 +9,21 @@ type Props = {
 
 export default function AuthLayout({ children, title }: Props) {
     return (
-        <main className='flex min-h-screen flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8'>
+        <main className='dark:bg-dm-primary bg-lm-primary flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8'>
             <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-                <div className='flex justify-center'>logo</div>
+                <div className='flex justify-center'>
+                    <RadarIcon className='size-16 text-primary' />
+                </div>
 
-                <h2 className='mt-6 text-center text-3xl font-light leading-9 text-gray-700'>
+                <Typography className='mt-6 text-center' variant='h2'>
                     {title}
-                </h2>
+                </Typography>
             </div>
 
             <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
-                <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>{children}</div>
+                <div className='dark:bg-dm-secondary bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
+                    {children}
+                </div>
             </div>
         </main>
     );
