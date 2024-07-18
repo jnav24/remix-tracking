c61c6ee895dfbd3938d360edeeb98de87183a77f
Justin Navarro <info@justinnavarro.net>
2024-07-18 16:04:23 -0400

    setup the ids from drizzle

--- db/schema/user.ts
+++ db/schema/user.ts
@@ -1,8 +1,8 @@
-import { mysqlTable, varchar, serial, datetime } from 'drizzle-orm/mysql-core';
-import { softDeletes, timestamps } from '../utils';
+import { mysqlTable, varchar, datetime } from 'drizzle-orm/mysql-core';
+import { ids, softDeletes, timestamps } from '../utils';
 
 export const user = mysqlTable('users', {
-    id: serial('id').primaryKey(),
+    ...ids,
     email: varchar('email', { length: 256 }),
     emailVerifiedAt: datetime('email_verified_at', { mode: 'date' }),
     password: varchar('password', { length: 256 }),
--- db/utils.ts
+++ db/utils.ts
@@ -1,5 +1,11 @@
-import { datetime } from 'drizzle-orm/mysql-core';
+import { datetime, serial, varchar } from 'drizzle-orm/mysql-core';
 import { sql } from 'drizzle-orm';
+import { createId } from '@paralleldrive/cuid2';
+
+export const ids = {
+    pk: serial('pk').primaryKey(),
+    id: varchar('id', { length: 128 }).$defaultFn(() => createId()),
+};
 
 export const timestamps = {
     createdAt: datetime('created_at', { mode: 'date', fsp: 0 })
