278335e9509a05030792e4062e4e617db635fcc8
Justin Navarro <info@justinnavarro.net>
2024-07-19 00:08:58 -0400

    update the env utils to get env types from the env.d.ts

---
+++ app/types/env.d.ts
@@ -0,0 +1,18 @@
+export type EnvVariables = {
+    APP_SLUG: string;
+    DB_HOST: string;
+    DB_PORT: string;
+    DB_USERNAME: string;
+    DB_PASSWORD: string;
+    DB_ROOT_PASSWORD: string;
+    DB_DATABASE: string;
+    DB_DATA_PATH: string;
+    REDIS_PRIMARY_PORT: string;
+    REDIS_REPLICA_PORT: string;
+    REDIS_DATA_PATH: string;
+    REDIS_PASSWORD: string;
+    SESSION_NAME: string;
+    SESSION_SECRET: string;
+    HASH_KEY: string;
+    ENCRYPT_SECRET: string;
+};
--- app/utils/env.ts
+++ app/utils/env.ts
@@ -1,21 +1,4 @@
-type EnvVariables = {
-    APP_SLUG: string;
-    DB_HOST: string;
-    DB_PORT: string;
-    DB_USERNAME: string;
-    DB_PASSWORD: string;
-    DB_ROOT_PASSWORD: string;
-    DB_DATABASE: string;
-    DB_DATA_PATH: string;
-    REDIS_PRIMARY_PORT: string;
-    REDIS_REPLICA_PORT: string;
-    REDIS_DATA_PATH: string;
-    REDIS_PASSWORD: string;
-    SESSION_NAME: string;
-    SESSION_SECRET: string;
-    HASH_KEY: string;
-    ENCRYPT_SECRET: string;
-};
+import { EnvVariables } from '~/types/env';
 
 export const getEnv = (value: keyof EnvVariables, defaultValue = '') => {
     if (typeof process !== 'undefined') {
