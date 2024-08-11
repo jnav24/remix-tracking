20bad28e7582944279d594fb630c55c188832bdb
Justin Navarro <info@justinnavarro.net>
2024-08-11 09:57:33 -0400

    created team page

---
+++ app/routes/dashboard.team.tsx
@@ -0,0 +1,9 @@
+import DashboardLayout from '~/components/layouts/DashboardLayout';
+
+export default function DashboardTeam() {
+    return (
+        <DashboardLayout title='Team'>
+            <h1>Heyo!</h1>
+        </DashboardLayout>
+    );
+}
