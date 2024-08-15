89cc49fd552f1eaf8d5a15155170015e73eddd92
Justin Navarro <info@justinnavarro.net>
2024-08-15 08:38:56 -0400

    created team page

--- app/routes/dashboard.team.tsx
+++ app/routes/dashboard.team.tsx
@@ -1,15 +1,35 @@
 import DashboardLayout from '~/components/layouts/DashboardLayout';
 import AddIcon from '~/components/icons/outline/AddIcon';
+import Modal from '~/components/modals/Modal';
+import { useState } from 'react';
+import InviteMemberModal from '~/components/modals/InviteMemberModal';
 
 export default function DashboardTeam() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <DashboardLayout
-            actions={[
-                { label: 'New Invite', icon: <AddIcon className='size-4' />, click: () => null },
-            ]}
-            title='Team'
-        >
-            <h1>Heyo!</h1>
-        </DashboardLayout>
+        <>
+            <Modal
+                showModal={isOpen}
+                closeModal={() => setIsOpen(false)}
+                hideCloseButton
+                persistent
+            >
+                <InviteMemberModal closeModal={() => setIsOpen(false)} />
+            </Modal>
+
+            <DashboardLayout
+                actions={[
+                    {
+                        label: 'New Invite',
+                        icon: <AddIcon className='size-4' />,
+                        click: () => setIsOpen(true),
+                    },
+                ]}
+                title='Team'
+            >
+                <h1>Heyo!</h1>
+            </DashboardLayout>
+        </>
     );
 }
