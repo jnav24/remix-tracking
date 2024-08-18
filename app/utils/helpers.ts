fec95304e7b6ecac45ac20a44f3d86f93efe409b
Justin Navarro <info@justinnavarro.net>
2024-08-18 08:20:00 -0400

    fixed typing issues with parseNested helper

--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -79,14 +79,14 @@ const getErrorMessage = (err: string | Error | unknown) => {
     return err;
 };
 
-const parseNested = (item: Record<string, any>, value: string) => {
-    return value.split('.').reduce((result, current) => {
-        if (result[current]) {
-            return result[current];
+const parseNested = <R extends object>(item: R, value: string): string => {
+    return value.split('.').reduce((result: string | R, current: string) => {
+        if (typeof result !== 'string' && result[current as keyof R]) {
+            return result[current as keyof R] as R;
         }
 
         return '';
-    }, item);
+    }, item) as string;
 };
 
 /**
